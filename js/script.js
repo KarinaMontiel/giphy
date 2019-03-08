14b1e1f5f4ba5d6e0e20ca18a2c331287d1bc8d8
Karina Montiel <karinamontiel375@gmail.com>
2019-03-08 02:09:56 +0000

    new

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@
 // let api_url="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $("#search-button").click(function(){
     let value=$("#search-term").val();
+    
   $.ajax({
         url: `https://api.giphy.com/v1/gifs/search?q=${value}&rating=pg&api_key=dc6zaTOxFJmzC`,
         method: "GET",
         success: function(response) {
-          $("body").append(`<img src="${response.data[0].images.original.url}">`);
+            let number=Math.floor(Math.random()*response.data.length);
+            console.log(number);
+          $(".gallery").append(`<img src="${response.data[number].images.original.url}">`);
         }
         
     }); 
-    $(".text-center").hide;
+    $(".text-center").hide();
   
 });
 
