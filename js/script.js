e96d13aa0bf4a5f6a08042e557691d8c217b3784
Karina Montiel <karinamontiel375@gmail.com>
2019-03-08 01:16:13 +0000

    new

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-let api_url="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
+// let api_url="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $("#search-button").click(function(){
-    
+    let value=$("#search-term").val();
   $.ajax({
-        url: api_url,
+        url: `https://api.giphy.com/v1/gifs/search?q=${value}&rating=pg&api_key=dc6zaTOxFJmzC`,
         method: "GET",
         success: function(response) {
           $("body").append(`<img src="${response.data[0].images.original.url}">`);
         }
         
     }); 
-  
+    $(".text-center").hide;
   
 });
 
